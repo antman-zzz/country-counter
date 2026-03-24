fe0076c72653c275394dbf798678083011e63001
antman-zzz <227376327+antman-zzz@users.noreply.github.com>
2026-03-24 12:44:03 +0900

    Fix: Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/country-counter/',
   plugins: [react()],
   resolve: {
     alias: {
